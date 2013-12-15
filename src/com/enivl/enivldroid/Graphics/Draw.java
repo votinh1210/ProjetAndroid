9fe6bd6aa76c1ab221aa8548ddd64fa20c98cb5c
votinh1210 <dinhvuminh.qb.vn@gmail.com>
2013-12-15 19:49:44 +0100

    changed graphics

--- src/com/enivl/enivldroid/Graphics/Draw.java
+++ src/com/enivl/enivldroid/Graphics/Draw.java
@@ -13,11 +13,16 @@ import android.graphics.Typeface;
 import android.view.View;
 
 public class Draw extends View{
+	private int xmin=0;
+	private int ymin=0;
+	private int xmax;
+	private int ymax;
+	
 	private Paint paint; 
 	
-	private float ballRadius = 80; // Ball's radius
-	private float ballX = ballRadius + 20;  // Ball's center (x,y)
-	private float ballY = ballRadius + 40;
+	private float ballRadius; // Ball's radius
+	private float ballX;  // Ball's center (x,y)
+	private float ballY;
 	private RectF ballBounds;      // Needed for Canvas.drawOval
 	
 	private int red, green, blue;
@@ -40,6 +45,10 @@ public class Draw extends View{
 		   red = Integer.parseInt(PollModbus.modbusValues[0].toString());
 		   green = Integer.parseInt(PollModbus.modbusValues[1].toString());
 		   blue = Integer.parseInt(PollModbus.modbusValues[2].toString());
+		   
+		   ballX=(xmax-xmin)/2;
+		   ballY=(ymax-ymin)/2;
+		   ballRadius=Math.min(ballX-20, ballY-40);
 		  
 	      ballBounds.set(ballX-ballRadius, ballY-ballRadius, ballX+ballRadius, ballY+ballRadius);
 		  int color = Color.rgb(red, green, blue);
@@ -47,7 +56,7 @@ public class Draw extends View{
 	      canvas.drawOval(ballBounds, paint);
 	      
 	   // Draw the status message
-	      paint.setColor(Color.CYAN);
+	      paint.setColor(Color.BLACK);
 	      
 	      statusMsg = "RGB("+red+","+green+","+blue+")";
 	      canvas.drawText(statusMsg, 10, 30, paint);
@@ -62,6 +71,12 @@ public class Draw extends View{
 	      } catch (InterruptedException e) { }
 	      
 	      invalidate();  // Force a re-draw
-	   }
+	}
+	
+	public void onSizeChanged(int w,int h,int oldw, int oldh){
+		xmax=w-1;
+		ymax=h-1;
+	}
+	   
 }
 
